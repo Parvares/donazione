c9f7799fb206be9bb9965d446c1c9076ad1d9376
Parvares <55709458+Parvares@users.noreply.github.com>
2024-12-22 11:25:12 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
+// Ottieni le variabili dall'oggetto window che verranno impostate dal workflow
 const config = {
-    EMAILJS_PUBLIC_KEY: process.env.EMAILJS_PUBLIC_KEY,
-    EMAILJS_SERVICE_ID: process.env.EMAILJS_SERVICE_ID,
-    EMAILJS_TEMPLATE_ID: process.env.EMAILJS_TEMPLATE_ID
+    EMAILJS_PUBLIC_KEY: window._env_.EMAILJS_PUBLIC_KEY,
+    EMAILJS_SERVICE_ID: window._env_.EMAILJS_SERVICE_ID,
+    EMAILJS_TEMPLATE_ID: window._env_.EMAILJS_TEMPLATE_ID
 };
 
 export default config;
